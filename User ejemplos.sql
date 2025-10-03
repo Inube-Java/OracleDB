6db029d226d66c6722e1f01fe34aeefb3c5c220a
urielSanchezMxGto <153027435+urielSanchezMxGto@users.noreply.github.com>
2025-10-03 11:28:28 -0600

    Add files via upload
    Script para crear un usuario en oracle

---
+++ User ejemplos.sql
@@ -0,0 +1,16 @@
+-- Crear el usuario
+CREATE USER ejemplos IDENTIFIED BY ejemplo
+DEFAULT TABLESPACE users
+TEMPORARY TABLESPACE temp
+QUOTA UNLIMITED ON users;
+
+-- Dar privilegios básicos para conectarse y trabajar con sus tablas
+GRANT CREATE SESSION TO ejemplos;  -- Permiso para hacer login
+GRANT CREATE TABLE TO ejemplos; --Permiso para crear tablas
+GRANT CREATE SEQUENCE TO ejemplos; -- Permiso para crear secuencias
+GRANT CREATE VIEW TO ejemplos; -- Permiso para crear vistas
+GRANT CREATE TRIGGER TO ejemplos;  -- Permioso para crear triggers
+GRANT CREATE PROCEDURE TO ejemplos; -- Permiso para crear funciones y procedimientos almacenados
+
+
+
